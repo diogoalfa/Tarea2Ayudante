54226aa4a5960d88c072ee68aaa08340c44c20b9
faxtork <sebastian.esparza@ceinf.cl>
2013-11-15 21:00:21 -0300

    validado html y css

--- index.php
+++ index.php
@@ -1,27 +1,28 @@
-<!DOCTYPE html>
-<html lang="en">
-
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd"> 
+<html>
 	<head>
 		<title>Tarea 2 ayudantia</title>
-		<meta charset="UTF-8">
-		<link rel="stylesheet" href="css/style.css">
-		<h1 id="h1">Grupo 01 - Ingeniería de Software</h1>	
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+		<meta http-Equiv="Content-Type" content="text/html; charset=iso-8859-1">
+		<link rel="stylesheet" href="css/style.css" type="text/css">
 		<!--Funcion para cargar aleatoriamente los dos eslogan en el <div> cuyo id="slogan"-->
-		<script >
+		<script type='text/javascript'>
 				var objetos = new Array();
 				//acá van los eslogan en fotos
 				objetos[0] = "img/slogan 1.png";
 				objetos[1] = "img/slogan 2.png";
-
-				function aleatorio() {
+		</script>
+		<script type='text/javascript'>
+						function aleatorio() {
 				   var azar = Math.floor(Math.random() * objetos.length);
 				   document.images["imagen"].src = objetos[azar];
 				}
 		</script>
 	</head>
 
-	<body id="body" onload="aleatorio()">
-    	<div id="slogan"><img src="" name="imagen" /></div>
+	<body onload="aleatorio()" class="body">
+			<h1 id="h1">Grupo 01 - Ingeniería de Software</h1>	
+    	<div id="slogan"><img src="" alt="" name="imagen"></div>
 		<h4 id="citas">"El ordenador nació para resolver problemas que antes no existían"<br>--Bill Gates<br><br>
 			"El hardware es lo que hace a una máquina rápida; el software es lo que hace que una máquina rápida se vuelva lenta"<br>--Craig Bruce
 		</h4>
@@ -29,7 +30,7 @@
 		<div id="form">
 			<h4>Seleccione un integrante para ver detalles</h4>
 			<form method="GET" action="<?php echo $_SERVER['PHP_SELF']; ?>">
-				<select name="integrantes" id="">
+				<select name="integrantes">
 					<option value="-1">Seleccionar</option>
 					<option value="0">Sebastian Esparza</option>
 					<option value="1">Diego Navia</option>
@@ -38,9 +39,7 @@
 				<input type="submit" value="Ver" name="btn_enviar">
 			</form>
 		</div>	
-	</body>
-
-	<?php
+		<?php
 	if(isset($_GET['btn_enviar']))
 		{
 			$nombre=$_GET['integrantes'];	
@@ -49,7 +48,7 @@
 			if($nombre==0)
 				echo '
 						<br><br><br><br>
-						<table id="Datos">
+						<table class="Datos">
 							<tr>
 								<td><b>Nombre</b></td>
 								<td><b>Edad</b></td>
@@ -61,16 +60,16 @@
 								<td>Sebastian Esparza</td>
 								<td>22</td>
 								<td>Ingeniería en Informática</td>
-								<td><a id="color" href="https://github.com/faxtork">Github</a></td>
-								<td><a id="color" href="https://www.facebook.com/sebastian.esparzag">Facebook</a></td>
+								<td><a href="https://github.com/faxtork">Github</a></td>
+								<td><a href="https://www.facebook.com/sebastian.esparzag">Facebook</a></td>
 							</tr>
-							<tr ><td id="colspan" colspan="5"> FOTO</td></tr>
+							<tr ><td colspan="5"><div ><img src="img/yo.jpg" alt="" id="fotoSeba"></div></td></tr>
 						</table>
 			';
 			if($nombre==1)
 				echo '
 						<br><br><br><br>
-						<table id="Datos">
+						<table class="Datos">
 							<tr>
 								<td><b>Nombre</b></td>
 								<td><b>Edad</b></td>
@@ -82,16 +81,16 @@
 								<td>Diego Navia</td>
 								<td>21</td>
 								<td>Ingeniería civil en Computación</td>
-								<td><a id="color" href="https://github.com/diogoalfa">Github</a></td>
-								<td><a id="color" href="https://www.facebook.com/diogo.again">Facebook</a></td>
+								<td><a href="https://github.com/diogoalfa">Github</a></td>
+								<td><a href="https://www.facebook.com/diogo.again">Facebook</a></td>
 							</tr>
-							<tr ><td id="colspan" colspan="5"><div ><img src="img/fotoMia.jpeg" id="fotoDiego" /></div></td></tr>
+							<tr ><td colspan="5"><div ><img src="img/fotoMia.jpeg" alt="" id="fotoDiego" ></div></td></tr>
 						</table>
 			';
 			if($nombre==2)
 				echo '
 						<br><br><br><br>
-						<table id="Datos" >
+						<table class="Datos" >
 							<tr>
 								<td><b>Nombre</b></td>
 								<td><b>Edad</b></td>
@@ -103,15 +102,25 @@
 								<td>Francisco Ramírez</td>
 								<td>21</td>
 								<td>Ingeniería Civil en Computación</td>
-								<td><a id="color" href="https://github.com/FcoHernan">FcoHernan</a></td>
-								<td><a id="color" href="https://www.facebook.com/Fco.Hernan">Francisco Hernan&apos;</a></td>
+								<td><a href="https://github.com/FcoHernan">FcoHernan</a></td>
+								<td><a href="https://www.facebook.com/Fco.Hernan">Francisco Hernan</a></td>
 							</tr>
-							<tr ><td id="colspan" colspan="5"><div ><img src="img/foto.jpg" id="fotoFco" /></div></td></tr>
-							<tr><td id="colspan" colspan="5"><a id="mail" href="mailto:Franciscoramirezfernandez@gmail.com">Enviar Mail</a></tr>
+							<tr ><td colspan="5"><div ><img src="img/foto.jpg" alt="" id="fotoFco" ></div></td></tr>
+							<tr><td colspan="5"><a id="mail" href="mailto:Franciscoramirezfernandez@gmail.com">Enviar Mail</a></tr>
 						</table>
 			';
 		}
 	?>
-        
-        
+    <p>
+	    <a href="http://validator.w3.org/check?uri=referer"><img
+	      src="http://www.w3.org/Icons/valid-html401" alt="Valid HTML 4.01 Transitional" height="31" width="88"></a>
+  	</p>
+  	<p>
+		<a href="http://jigsaw.w3.org/css-validator/check/referer">
+		    <img style="border:0;width:88px;height:31px"
+		        src="http://jigsaw.w3.org/css-validator/images/vcss-blue"
+		        alt="" >
+		</a>
+	</p>
+  </body>       
 </html>
